27b0bec8797adcac2c9e891a26bbe8073015a312
fraol lemma <fraollemma0506@gmail.com>
2025-05-20 01:53:22 +0300

    Allow sesssion and cookies

--- project/settings.py
+++ project/settings.py
@@ -79,8 +79,11 @@ CORS_ALLOW_ALL_ORIGINS = True
 ROOT_URLCONF = 'project.urls'
 
                 # for production
- SESSION_COOKIE_DOMAIN = '.railway.app'
- CSRF_COOKIE_DOMAIN = '.railway.app'
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+                # for production
+SESSION_COOKIE_DOMAIN = '.railway.app'
+CSRF_COOKIE_DOMAIN = '.railway.app'
                  # for development
 # SESSION_COOKIE_DOMAIN = None
 # CSRF_COOKIE_DOMAIN    = None
