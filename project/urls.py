d888b3214402e7823d4a38e485ed684482fe0a0f
fraol lemma <fraollemma0506@gmail.com>
2025-05-23 10:32:14 +0300

    Message 1

--- project/urls.py
+++ project/urls.py
@@ -1,4 +1,27 @@
 # project/urls.py
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from django.contrib import admin
+# from django.urls import path, include
+# from django.contrib.auth import views as auth_views
+# from django.conf.urls.i18n import i18n_patterns
+
+# urlpatterns = [
+#     path('i18n/', include('django.conf.urls.i18n')),
+# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# urlpatterns += i18n_patterns(
+#     path('admin/', admin.site.urls),
+#     path('', include('base.urls')),
+#     path('users/', include('users.urls')),
+#     path('items/', include('items.urls')),
+#     path('conversation/', include('conversation.urls')),
+#     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+#     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+#     path('contact/', include('contact.urls')),
+# )
+
+# project/urls.py
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -8,7 +31,7 @@ from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
@@ -20,3 +43,6 @@ urlpatterns += i18n_patterns(
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('contact/', include('contact.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
