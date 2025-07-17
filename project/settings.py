fe66f5873aebce375be4cd263a4d32a2be14d9af
fraol lemma <fraollemma0506@gmail.com>
2025-07-17 14:05:16 +0300

    fix vs 1

--- project/settings.py
+++ project/settings.py
@@ -64,7 +64,14 @@ INSTALLED_APPS = [
     'companies',
     'vehicles',
     'clothings',
+
+    # aditional
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5" 
+CRISPY_TEMPLATE_PACK = "bootstrap5" 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
